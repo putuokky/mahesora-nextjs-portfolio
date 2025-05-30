244aa33308a309878055591b0c246e3c1265968a
putuokky <okkymahes@gmail.com>
2025-05-30 20:03:24 +0800

    update mail

--- components/RightSide.tsx
+++ components/RightSide.tsx
@@ -3,7 +3,7 @@ const RightSide = () => {
     return (
         <div className="flex flex-col items-center justify-end w-full h-full gap-6 text-textLight">
             <a href="mailto:okkymahes@gmail.com">
-                <p className="text-sm tracking-wide rotate-90 w-72 text-textGreen">reactbd.proton.me</p>
+                <p className="text-sm tracking-wide rotate-90 w-72 text-textGreen">okkymahes@gmail.com</p>
             </a>
             <span className="w-[2px] h-32 bg-textDark inline-flex"></span>
         </div>
