ab56326b2ccc63c6701c96336edf54cb2e30d069
putuokky <okkymahes@gmail.com>
2025-05-31 11:36:28 +0800

    update link api social

--- app/api/socials/route.tsx
+++ app/api/socials/route.tsx
@@ -5,7 +5,7 @@ const socials = [
     { id: 1, nama: 'facebook', link: 'https://www.facebook.com/okkymahesora' },
     { id: 2, nama: 'twitter', link: 'https://x.com/okkymhwr' },
     { id: 3, nama: 'instagram', link: 'https://instagram.com/okkymahesora' },
-    { id: 4, nama: 'linkedin', link: 'https://instagram.com/okkymahesora' },
+    { id: 4, nama: 'linkedin', link: 'https://www.linkedin.com/in/okkymahesora/' },
     // { id: 5, nama: 'tiktok', link: 'https://www.tiktok.com/@vannemplo' },
     { id: 6, nama: 'github', link: 'https://github.com/putuokky' },
     { id: 7, nama: 'discord', link: 'https://discord.gg/JNSGXMaEWE' },
